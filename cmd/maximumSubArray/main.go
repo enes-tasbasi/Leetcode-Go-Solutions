754c0930cbc3ade6defc890aaebc6d164faa0687
= <=>
2021-12-21 21:13:32 -0500

    Working solution from the internet

--- cmd/maximumSubArray/main.go
+++ cmd/maximumSubArray/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	nums := []int{-2, 1, -3, 4, -1, 2, 1, -5, 4}
@@ -8,48 +10,27 @@ func main() {
 	fmt.Println(maxSubArray(nums))
 }
 
-// Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
-func maxSubArray(nums []int) int {
-	return sumArr(compare(nums))
-}
+func max(nums ...int) int {
+	max := nums[0]
 
-func sumArr(arr []int) int {
-	sum := 0
-	for _, v := range arr {
-		sum += v
-	}
-
-	return sum
-}
-
-func max(nums map[int][]int) []int {
-	max := -10001
-
-	for k := range nums {
-		if k > max {
-			max = k
+	for _, v := range nums {
+		if v > max {
+			max = v
 		}
 	}
 
-	return nums[max]
+	return max
 }
 
-func compare(subArr []int) []int {
-	if len(subArr) == 1 {
-		return subArr
+// Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+func maxSubArray(nums []int) int {
+	maxSoFar := nums[0]
+	maxEndingHere := nums[0]
+
+	for i := 1; i < len(nums); i++ {
+		maxEndingHere = max(maxEndingHere+nums[i], nums[i])
+		maxSoFar = max(maxSoFar, maxEndingHere)
 	}
 
-	currSum := sumArr(subArr)
-
-	leftSub := subArr[:len(subArr)/2]
-	highestLeft := compare(leftSub)
-
-	rightSub := subArr[len(subArr)/2:]
-	highestRight := compare(rightSub)
-
-	return max(map[int][]int{
-		sumArr(highestLeft):  highestLeft,
-		sumArr(highestRight): highestRight,
-		currSum:              subArr,
-	})
+	return maxSoFar
 }
